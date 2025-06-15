584db92e136564b1e1e772ef3e1061afec4fc295
maxmulla23 <maxmulla23@gmail.com>
2025-06-15 13:04:54 +0300

    product

--- ecommerce mvc/Models/Product.cs
+++ ecommerce mvc/Models/Product.cs
@@ -7,6 +7,14 @@ namespace ecommerce_mvc.Models
 {
     public class Product
     {
-        
+    public int ProductId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
     }
 }

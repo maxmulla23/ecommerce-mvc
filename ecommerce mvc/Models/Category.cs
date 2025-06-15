385635ea70cd13073affc9897b8675893f1fc8bc
maxmulla23 <maxmulla23@gmail.com>
2025-06-15 13:14:37 +0300

    category

--- ecommerce mvc/Models/Category.cs
+++ ecommerce mvc/Models/Category.cs
@@ -7,6 +7,9 @@ namespace ecommerce_mvc.Models
 {
     public class Category
     {
-        
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Product> Products { get; set; }
     }
 }
--- ecommerce mvc/Models/Product.cs
+++ ecommerce mvc/Models/Product.cs
@@ -8,13 +8,13 @@ namespace ecommerce_mvc.Models
     public class Product
     {
     public int ProductId { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string ImageUrl { get; set; }
-    public decimal Price { get; set; }
-    public int StockQuantity { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
 
-    public int CategoryId { get; set; }
-    public Category Category { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
